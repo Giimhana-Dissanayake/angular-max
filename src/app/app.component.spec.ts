f87534827c82635ff4fff2e0336290c7671606f0
Gimhana Dissanayake <46950713+Gimhana-Dissanayake@users.noreply.github.com>
2021-09-17 09:33:36 +0530

    section-3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-max'`, () => {
+  it(`should have as title 'angular-max-section-three'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-max');
+    expect(app.title).toEqual('angular-max-section-three');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-max app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-max-section-three app is running!');
   });
 });
